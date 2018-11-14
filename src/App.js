048ce6c258de1555da375c43aa7f75442b8bc08d
Ryan Graff <ryan.graff@bedrockdata.com>
2018-11-14 11:47:09 -0500

    chore: Initial commit.

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import {hot} from 'react-hot-loader';
+import React, {PureComponent} from 'react';
+import Style from './App.less';
+
+const {App: ClassName} = Style;
+
+export class App extends PureComponent {
+  render() {
+    return (
+      < div
+        className={`App ${ClassName}`}
+      >
+        <div
+          className='Box'
+        >
+          <input
+            type='text'
+            defaultValue='Demo Text Input'
+          />
+        </div>
+        <br/>
+        <br/>
+        <div
+          className='Box'
+        >
+          <code>
+            Demo Code Text
+          </code>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
